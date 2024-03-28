c64049825277c4deedbf85796f8bfa0cfedb60ff
ElStabilini <el.stabilini@gmail.com>
2024-03-28 17:51:14 +0100

    Updated graphtools.py - still need to sobstitute metis

--- graphtools.py
+++ graphtools.py
@@ -4,3 +4,71 @@ import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
 
+def euclidean_distance(a, b):
+    return np.linalg.norm(np.array(a) - np.array(b))
+
+#Build k-NN graph
+
+def knn_graph(df, k, verbose=False):
+    points = [p[1:] for p in df.itertuples()]
+    g = nx.Graph()
+    for i in range(0, len(points)):
+        g.add_node(i)
+    if verbose:
+        print("Building kNN graph (k = %d)..." % (k))
+    iterpoints = tqdm(enumerate(points), total=len(
+        points)) if verbose else enumerate(points)
+    for i, p in iterpoints:
+        distances = list(map(lambda x: euclidean_distance(p, x), points))
+        closests = np.argsort(distances)[1:k+1]  # second trough kth closest
+        # print(distances[0])
+        for c in closests:
+            g.add_edge(i, c, weight=1.0 / distances[c], similarity=int(
+                1.0 / distances[c] * 1e4))
+        g.node[i]['pos'] = p
+    g.graph['edge_weight_attr'] = 'similarity'
+    return g
+
+#the part_graph code is still to modify because I need it to run with networkx instead of metis
+def part_graph(graph, k, df=None):
+    edgecuts, parts = metis.part_graph(
+        graph, 2, objtype='cut', ufactor=250)
+    # print(edgecuts)
+    for i, p in enumerate(graph.nodes()):
+        graph.node[p]['cluster'] = parts[i]
+    if df is not None:
+        df['cluster'] = nx.get_node_attributes(graph, 'cluster').values()
+    return graph
+
+def get_cluster(graph, clusters):
+    nodes = [n for n in graph.node if graph.node[n]['cluster'] in clusters]
+    return nodes
+
+
+def connecting_edges(partitions, graph):
+    cut_set = []
+    for a in partitions[0]:
+        for b in partitions[1]:
+            if a in graph:
+                if b in graph[a]:
+                    cut_set.append((a, b))
+    return cut_set
+
+
+def min_cut_bisector(graph):
+    graph = graph.copy()
+    graph = part_graph(graph, 2)
+    partitions = get_cluster(graph, [0]), get_cluster(graph, [1])
+    return connecting_edges(partitions, graph)
+
+
+def get_weights(graph, edges):
+    return [graph[edge[0]][edge[1]]['weight'] for edge in edges]
+
+
+def bisection_weights(graph, cluster):
+    cluster = graph.subgraph(cluster)
+    edges = min_cut_bisector(cluster)
+    weights = get_weights(cluster, edges)
+    return weights
+
